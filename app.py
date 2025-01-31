e364245971cc6ece65d859b90fb1fe1ccc884fbf
Igboke Daniel <danieligboke669@gmail.com>
2025-01-31 18:51:23 +0100

    Added timezone requirement and explicit ok status code

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify
-from datetime import datetime
+from datetime import datetime,timezone
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,12 +7,12 @@ CORS(app)
 
 @app.route('/')
 def get_info():
-    current_datetime = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
+    current_datetime = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
     return jsonify({
         "email": "danieligboke669@gmail.com",
         "current_datetime": current_datetime,
         "github_url": "https://github.com/Igboke/api-project"
-    })
+    }),200
 
 if __name__ == '__main__':
     app.run()
